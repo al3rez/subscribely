03996de0463c9a97b7d05b506296c4c75ff78e6e
Alireza Bashiri <al3rez@gmail.com>
2023-12-10 00:12:30 +0700

    add speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <SpeedInsights />
+      </body>
     </html>
   );
 }
