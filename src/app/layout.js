222f802ce0fa742ad02e77779088bfe951b8e488
Alireza Bashiri <al3rez@gmail.com>
2023-12-10 00:19:47 +0700

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children} <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
