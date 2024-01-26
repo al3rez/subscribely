6da8ad0cd5e763c2ac1f579a3c19e43e97ed220b
Alireza Bashiri <al3rez@gmail.com>
2024-01-26 21:48:47 +0700

    add sa

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({
@@ -17,8 +16,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" style={inter.style}>
       <body>
-        {children} <SpeedInsights />
-        <Analytics />
+        {children}
+        <SpeedInsights />
+        <script
+          async
+          defer
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
+        <noscript>
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif"
+            alt=""
+            referrerpolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
       </body>
     </html>
   );
